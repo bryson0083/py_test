dfaf54aec9e52d1e06b3796fcfe48f6274caaab4
bryson0083 <bryson0083@gmail.com>
2017-02-16 16:52:40 +0800

    modify 20170216

--- 20170210-01.py
+++ 20170210-01.py
@@ -12,7 +12,7 @@ import time
 
 def READ_DAY_QUO(arg_sear_comp_id, arg_date_st, arg_date_ed):
 
-	#讀取周線開盤價
+	#讀取月線開盤價
 	strsql  = "select OPEN from STOCK_QUO "
 	strsql += "where "
 	strsql += "SEAR_COMP_ID = '" + arg_sear_comp_id + "' and "
@@ -26,14 +26,14 @@ def READ_DAY_QUO(arg_sear_comp_id, arg_date_st, arg_date_ed):
 	result = cursor.fetchone()
 
 	if result[0] is not None:
-		week_open = result[0]
+		mon_open = result[0]
 	else:
-		week_open = 0
+		mon_open = 0
 
 	#關閉cursor
 	cursor.close()
 
-	#讀取周線收盤價，與當周最後收盤日期
+	#讀取月線收盤價，與當月最後收盤日期
 	strsql  = "select CLOSE, QUO_DATE from STOCK_QUO "
 	strsql += "where "
 	strsql += "SEAR_COMP_ID = '" + arg_sear_comp_id + "' and "
@@ -47,16 +47,16 @@ def READ_DAY_QUO(arg_sear_comp_id, arg_date_st, arg_date_ed):
 	result = cursor.fetchone()
 
 	if result[0] is not None:
-		week_close = result[0]
-		week_close_dt = result[1]
+		mon_close = result[0]
+		mon_close_dt = result[1]
 	else:
-		week_close = 0
-		week_close_dt = ""
+		mon_close = 0
+		mon_close_dt = ""
 
 	#關閉cursor
 	cursor.close()
 
-	#讀取周線最低價
+	#讀取月線最低價
 	strsql  = "select min(LOW) from STOCK_QUO "
 	strsql += "where "
 	strsql += "SEAR_COMP_ID = '" + arg_sear_comp_id + "' and "
@@ -68,14 +68,14 @@ def READ_DAY_QUO(arg_sear_comp_id, arg_date_st, arg_date_ed):
 	result = cursor.fetchone()
 
 	if result[0] is not None:
-		week_low = result[0]
+		mon_low = result[0]
 	else:
-		week_low = 0
+		mon_low = 0
 
 	#關閉cursor
 	cursor.close()
 
-	#讀取周線最高價
+	#讀取月線最高價
 	strsql  = "select max(HIGH) from STOCK_QUO "
 	strsql += "where "
 	strsql += "SEAR_COMP_ID = '" + arg_sear_comp_id + "' and "
@@ -87,14 +87,14 @@ def READ_DAY_QUO(arg_sear_comp_id, arg_date_st, arg_date_ed):
 	result = cursor.fetchone()
 
 	if result[0] is not None:
-		week_high = result[0]
+		mon_high = result[0]
 	else:
-		week_high = 0
+		mon_high = 0
 
 	#關閉cursor
 	cursor.close()
 
-	#讀取周線成交量
+	#讀取月線成交量
 	strsql  = "select sum(VOL) from STOCK_QUO "
 	strsql += "where "
 	strsql += "SEAR_COMP_ID = '" + arg_sear_comp_id + "' and "
@@ -106,41 +106,41 @@ def READ_DAY_QUO(arg_sear_comp_id, arg_date_st, arg_date_ed):
 	result = cursor.fetchone()
 
 	if result[0] is not None:
-		week_vol = result[0]
+		mon_vol = result[0]
 	else:
-		week_vol = 0
+		mon_vol = 0
 
 	#關閉cursor
 	cursor.close()
 
-	print(arg_sear_comp_id + ",open=" + str(week_open) + ", close=" + str(week_close) + ",high=" + str(week_high) + ",low=" + str(week_low) + ",vol=" + str(week_vol) + ",lat_dt=" + week_close_dt)
+	print(arg_sear_comp_id + ",open=" + str(mon_open) + ", close=" + str(mon_close) + ",high=" + str(mon_high) + ",low=" + str(mon_low) + ",vol=" + str(mon_vol) + ",lat_dt=" + mon_close_dt)
 
 	# 最後維護日期時間
 	str_date = str(datetime.datetime.now())
 	date_last_maint = parser.parse(str_date).strftime("%Y%m%d")
 	time_last_maint = parser.parse(str_date).strftime("%H%M%S")
-	prog_last_maint = "WEEK_QUO"
+	prog_last_maint = "QUO_MONTH"
 
-	#if week_close > 0 and week_vol > 0:
+	#if mon_close > 0 and mon_vol > 0:
 	#資料存檔
-	strsql  = "delete from STOCK_QUO_WEEKLY "
+	strsql  = "delete from STOCK_QUO_MONTH "
 	strsql += "where "
 	strsql += "SEAR_COMP_ID = '" + arg_sear_comp_id + "' and "
-	strsql += "QUO_DATE = '" + week_close_dt + "' "
+	strsql += "QUO_DATE = '" + mon_close_dt + "' "
 
 	conn.execute(strsql)
 
-	strsql  = "insert into STOCK_QUO_WEEKLY ("
+	strsql  = "insert into STOCK_QUO_MONTH ("
 	strsql += "SEAR_COMP_ID,QUO_DATE,OPEN,HIGH,LOW,CLOSE,VOL,"
 	strsql += "DATE_LAST_MAINT,TIME_LAST_MAINT,PROG_LAST_MAINT"
 	strsql += ") values ("
 	strsql += "'" + arg_sear_comp_id + "', "
-	strsql += "'" + week_close_dt + "', "
-	strsql += str(week_open) + ", "
-	strsql += str(week_high) + ", "
-	strsql += str(week_low) + ", "
-	strsql += str(week_close) + ", "
-	strsql += str(week_vol) + ", "
+	strsql += "'" + mon_close_dt + "', "
+	strsql += str(mon_open) + ", "
+	strsql += str(mon_high) + ", "
+	strsql += str(mon_low) + ", "
+	strsql += str(mon_close) + ", "
+	strsql += str(mon_vol) + ", "
 	strsql += "'" + date_last_maint + "', "
 	strsql += "'" + time_last_maint + "', "
 	strsql += "'" + prog_last_maint + "' "
@@ -150,7 +150,12 @@ def READ_DAY_QUO(arg_sear_comp_id, arg_date_st, arg_date_ed):
 	conn.commit()
 
 
-def READ_WEEK_QUO(arg_date_st, arg_date_ed):
+def READ_MONTH_QUO(arg_date):
+	arg_date_st = arg_date
+	arg_date_ed = arg_date[0:6] + "31"
+
+	#print(arg_date_st + "~" + arg_date_ed)
+
 	strsql  = "select distinct SEAR_COMP_ID from STOCK_QUO "
 	strsql += "where "
 	strsql += "QUO_DATE >= '" + arg_date_st + "' and "
@@ -176,7 +181,7 @@ def READ_WEEK_QUO(arg_date_st, arg_date_ed):
 ############################################################################
 # Main                                                                     #
 ############################################################################
-print("Executing QUO_WEEK...")
+print("Executing QUO_MONTH...")
 
 #建立資料庫連線
 conn = sqlite3.connect("market_price.sqlite")
@@ -185,11 +190,9 @@ conn = sqlite3.connect("market_price.sqlite")
 dt = datetime.datetime.now()
 dt2 = parser.parse(str(dt)).strftime("%Y%m%d")
 now_date = parser.parse(str(dt)).strftime("%Y/%m/%d")
-#now_date = "2017/02/08"
-#print(now_date)
 
 #LOG檔
-name = "QUO_WEEK_" + dt2 + ".txt"
+name = "QUO_MONTH_" + dt2 + ".txt"
 file = open(name, 'a', encoding = 'UTF-8')
 
 #程式運行過程計時開始
@@ -197,7 +200,7 @@ tStart = time.time()#計時開始
 file.write("\n\n\n*** LOG datetime  " + str(datetime.datetime.now()) + " ***\n")
 
 #設定結轉起始日期
-strsql = "select MAX(QUO_DATE) from STOCK_QUO_WEEKLY "
+strsql = "select MAX(QUO_DATE) from STOCK_QUO_MONTH "
 
 cursor = conn.execute(strsql)
 result = cursor.fetchone()
@@ -210,9 +213,9 @@ if result[0] is not None:
 	start_date = datetime.datetime.strptime(result[0], "%Y%m%d").date()
 
 	#for test 手動指定起始日期
-	#start_date = datetime.datetime.strptime('20080101', "%Y%m%d").date()
+	#start_date = datetime.datetime.strptime('20170101', "%Y%m%d").date()
 
-	start_date = start_date + relativedelta(days=-7)
+	start_date = start_date + relativedelta(days=-31)
 	start_date = str(start_date)[0:10]
 	start_date = parser.parse(start_date).strftime("%Y/%m/%d")
 else:
@@ -221,50 +224,33 @@ else:
 #關閉cursor
 cursor.close()
 
-#sys.exit("end of test...")
-
-#計算區間的天數，作為迴圈終止條件
+#計算區間的月份數，作為迴圈終止條件
 a = datetime.datetime.strptime(start_date, "%Y/%m/%d")
 b = datetime.datetime.strptime(now_date, "%Y/%m/%d")
-delta = b - a
-int_diff_date = delta.days + 1
-#print("days=" + str(int_diff_date) + "\n")
+#print(str(a) + "~" + str(b) + "\n")
+
+int_diff_month = (b.year * 12 + b.month) - (a.year * 12 + a.month) + 1
+#print("months=" + str(int_diff_month) + "\n")
+#sys.exit("end of test...")
 
 i = 1
 dt = ""
-duration_st = ""
-duration_ed = ""
-while i <= int_diff_date:
+duration_mon = ""
+while i <= int_diff_month:
 	#print(str(i) + "\n")
 	if i==1:
-		str_date = start_date
+		str_date = start_date[0:8] + "01"
 	else:
 		str_date = parser.parse(str(dt)).strftime("%Y/%m/%d")
-	
-	#判斷該日期是星期幾(1~7表示周一到周日)
-	dt_wday = datetime.datetime.strptime(str_date, '%Y/%m/%d').date().isoweekday()
-	#print(str_date + "   " + str(dt_wday) + "\n")
 
-	cal_yn = "N"
-	if dt_wday == 1:
-		duration_st = parser.parse(str(str_date)).strftime("%Y%m%d")
-	elif duration_st > "" and dt_wday == 7:
-		duration_ed = parser.parse(str(str_date)).strftime("%Y%m%d")
-		cal_yn = "Y"
-	else:
-		duration_ed = parser.parse(str(str_date)).strftime("%Y%m%d")
+	duration_mon = str_date.replace("/","")
 
-	if i == int_diff_date:
-		cal_yn = "Y"
+	#計算月線
+	READ_MONTH_QUO(duration_mon)
 
-	#若cal_yn為Y，結算一次周K
-	if cal_yn == "Y":
-		#print("區間開始於" + duration_st + "~" + duration_ed + "\n")
-		READ_WEEK_QUO(duration_st, duration_ed)
-
-	#日期往後推一天
+	#日期往後推一個月
 	dt = datetime.datetime.strptime(str_date, "%Y/%m/%d").date()
-	dt = dt + relativedelta(days=1)
+	dt = dt + relativedelta(months=1)
 	i += 1
 
 #關閉資料庫連線
---
+++ py_test.bat
@@ -0,0 +1,8 @@
+cd /d d:\py_test
+call activate py34
+python 20170206-02.py
+python 20170210-01.py
+call deactivate
+pause
+
+
