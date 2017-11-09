48d4af8b08a047a7b68865d199f8b7ec76c9b912
bryson0083 <bryson0083@gmail.com>
2017-11-09 16:55:32 +0800

    modify 20171109 V1

---
+++ 20171108-01.py
@@ -0,0 +1,5 @@
+def MAIN_A():
+	print(__file__)
+
+if __name__ == '__main__':
+	MAIN_A()
---
+++ 20171108-02.py
@@ -0,0 +1,7 @@
+import test_func as a
+import os
+
+#print(__file__)
+a.test_a()
+
+#print(os.path.basename(__file__))
